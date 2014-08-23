20365db07231111c47d125123d461d24acfc86bd
Justin Drees <jzdrees@gmail.com>
2014-08-23 14:57:15 +0200

    Update static pages & tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -21,16 +21,16 @@ describe "Static pages" do
 		expect(page).to have_title(full_title(''))
 		click_link "Sign up"
 		expect(page).to have_title(full_title('Sign up'))
-		click_link "Dwelling"
+		click_link "Oporto1"
 		expect(page).to have_title(full_title(''))
 	end
 
 	describe "Home page" do
 		before { visit root_path }
-		let(:heading)		{ 'Dwelling' }
+		let(:heading)		{ 'Oporto1' }
 		let(:page_title)	{ '' }
 
-		it_should_behave_like "all static pages"
+		#it_should_behave_like "all static pages"
 		it { should_not have_title('| Home') }
 	end
 
